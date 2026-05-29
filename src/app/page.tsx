35603b8f4747d0c5760876b53301a0a814e0af3e
manujo69 <manujo69@gmail.com>
2026-05-29 15:30:09 +0200

    Remove 'use client'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import dynamic from 'next/dynamic'
 import { LineaSelector } from '@/features/emt/components/LineaSelector'
 import { SentidoFilter } from '@/features/emt/components/SentidoFilter'
